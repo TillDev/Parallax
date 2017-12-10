c3d9eb90fd9288085da4dec6537cbb85c7822fe9
Любимый <andretill@till.one>
2017-12-10 18:55:10 +0300

    нене

---
+++ DerivedData/Parallax/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pages.build/DerivedSources/Pages_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char PagesVersionString[];
+ extern const double PagesVersionNumber;
+
+ const unsigned char PagesVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pages  PROJECT:Pods-1" "\n";
+ const double PagesVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/Parallax/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-Parallax.build/DerivedSources/Pods_Parallax_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_ParallaxVersionString[];
+ extern const double Pods_ParallaxVersionNumber;
+
+ const unsigned char Pods_ParallaxVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Parallax  PROJECT:Pods-1" "\n";
+ const double Pods_ParallaxVersionNumber __attribute__ ((used)) = (double)1.;
